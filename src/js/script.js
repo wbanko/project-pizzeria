41ac9fd2ae4cfd891ff670c4c08ff75a7cd980f7
Weronika Banko <w.b.banko@gmail.com>
2025-06-03 15:04:47 +0200

    add changes to value widget

--- src/js/script.js
+++ src/js/script.js
@@ -62,9 +62,10 @@ const select = {
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
       
-      console.log('newProduct:', thisProduct);
+      //console.log('newProduct:', thisProduct);
     }
     renderInMenu(){
         const thisProduct = this;
@@ -87,6 +88,12 @@ const select = {
         thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
         thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
         thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+        thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
+  }
+  initAmountWidget(){
+    const thisProduct = this;
+
+    thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
   }
 
     initAccordion(){
@@ -109,7 +116,7 @@ const select = {
     }
     initOrderForm(){
       const thisProduct = this;
-      console.log('initOrderForm')
+      //console.log('initOrderForm')
 
       thisProduct.form.addEventListener('submit', function(event){
       event.preventDefault();
@@ -129,10 +136,10 @@ const select = {
     }
     processOrder(){
       const thisProduct = this;
-      console.log('processOrder')
+      //console.log('processOrder')
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
+      //console.log('formData', formData);
       // set price to default price
       let price = thisProduct.data.price;
 
@@ -140,13 +147,13 @@ const select = {
       for(let paramId in thisProduct.data.params) {
       // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
       const param = thisProduct.data.params[paramId];
-      console.log(paramId, param);
+      //console.log(paramId, param);
 
       // for every option in this category
       for(let optionId in param.options) {
       // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
       const option = param.options[optionId];
-      console.log(optionId, option);
+      //console.log(optionId, option);
 
       // check if there is param with a name of paramId in formData and if it includes optionId
       if(formData[paramId] && formData[paramId].includes(optionId)) {
@@ -179,7 +186,55 @@ const select = {
   thisProduct.priceElem.innerHTML = price;
   }
 }
+class AmountWidget{
+  constructor(element){
+    const thisWidget = this;
 
+    console.log('AmountWidget:', thisWidget);
+    console.log('constructor aruments:', element);
+
+    thisWidget.getElements(element);
+    thisWidget.setValue(thisWidget.input.value);
+    thisWidget.initActions();
+
+  }
+  getElements(element){
+  const thisWidget = this;
+
+  thisWidget.element = element;
+  thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+  thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+  thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+}
+  setValue(value){
+    const thisWidget = this;
+
+    const newValue = parseInt(value);
+
+    /*TODO: Add validation*/
+if(thisWidget.value !== newValue && !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin &&
+    newValue <= settings.amountWidget.defaultMax){
+      thisWidget.value = newValue;
+    }
+
+    thisWidget.input.value=thisWidget.value;
+  }
+  initActions(){
+    const thisWidget = this;
+
+    thisWidget.input.addEventListener('change', function(){
+      thisWidget.setValue(thisWidget.input.value);
+    })
+    thisWidget.linkDecrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value -1);
+    })
+    thisWidget.linkIncrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value +1);
+    })
+  }
+}
    const app = {
 
     initMenu: function(){
@@ -213,4 +268,3 @@ const select = {
 
   app.init();
 }
-:)
